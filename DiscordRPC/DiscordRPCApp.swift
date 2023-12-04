ae3398b0442b426f50a3f4d3f821f25b33a6b13b
Deltaion Lee <32529306+MCMi460@users.noreply.github.com>
2023-12-03 19:51:22 -0600

    Initial Commit

---
+++ DiscordRPC/DiscordRPCApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiscordRPCApp.swift
+//  DiscordRPC
+//
+//  Created by Deltaion Lee on 12/3/23.
+//
+
+import SwiftUI
+
+@main
+struct DiscordRPCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
